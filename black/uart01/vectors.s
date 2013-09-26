18eff8cdb2897c1133bea80fbbce8778da4732d6
dwelch <dwelch@dwelch.com>
2013-09-25 22:55:59 -0400

    adding beaglebone black examples

---
+++ black/uart01/vectors.s
@@ -0,0 +1,45 @@
+
+/*vectors.s*/
+
+.globl _start
+_start:
+
+    mov sp,#0x83000000
+    bl notmain
+hang:   b .
+
+
+.globl dummy
+dummy:
+    bx lr
+
+.globl PUT8
+PUT8:
+    strb r1,[r0]
+    bx lr
+
+.globl GET8
+GET8:
+    ldrb r0,[r0]
+    bx lr
+
+.globl PUT16
+PUT16:
+    strh r1,[r0]
+    bx lr
+
+.globl GET16
+GET16:
+    ldrh r0,[r0]
+    bx lr
+
+
+.globl PUT32
+PUT32:
+    str r1,[r0]
+    bx lr
+
+.globl GET32
+GET32:
+    ldr r0,[r0]
+    bx lr
